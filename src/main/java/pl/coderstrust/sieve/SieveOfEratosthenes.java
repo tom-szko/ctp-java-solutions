0be087a9c0546dd8b93b45143727e94e275d3df1
Tomasz Szkopinski <tomasz.szkopinski@gmail.com>
2018-08-04 12:33:31 +0200

    Task #9 - extracted methods for counting and filling array with non-zero values and adjusted logic - replaced zeroes in the code with private static final variable 'ZERO' - improved naming of the variables

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -3,42 +3,51 @@ package pl.coderstrust.sieve;
 import java.util.Arrays;
 
 public class SieveOfEratosthenes {
+    private static final int ZERO = 0;
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sieve(123)));
     }
 
     private static int[] sieve(int maximumNumber) {
-        int[] arrayOfNumbers = new int[maximumNumber - 1];
-        fillArrayWithNumbers(arrayOfNumbers, 2);
-
+        int[] numbers = new int[maximumNumber - 1];
+        fillArrayWithNumbers(numbers, 2);
         for (int j = 2; j <= (int) Math.sqrt(maximumNumber); j++) {
             for (int k = j; k < maximumNumber - 1; k++) {
-                if (arrayOfNumbers[k] % j == 0) {
-                    arrayOfNumbers[k] = 0;
+                if (numbers[k] % j == ZERO) {
+                    numbers[k] = ZERO;
                 }
             }
         }
-        int numberOfPrimes = 0;
-        for (int item : arrayOfNumbers) {
-            if (item != 0) {
+        int[] primes = new int[countNonZeroValues(numbers)];
+        return fillArrayWithNonZeroValues(numbers, primes);
+    }
+
+    private static void fillArrayWithNumbers(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = number;
+            number++;
+        }
+    }
+
+    private static int countNonZeroValues(int[] array) {
+        int numberOfPrimes = ZERO;
+        for (int number : array) {
+            if (number != ZERO) {
                 numberOfPrimes++;
             }
         }
-        int[] arrayOfPrimes = new int[numberOfPrimes];
-        int count = 0;
-        for (int item : arrayOfNumbers) {
-            if (item != 0) {
-                arrayOfPrimes[count] = item;
+        return numberOfPrimes;
+    }
+
+    private static int[] fillArrayWithNonZeroValues(int[] arrayWithZeroes, int[] arrayWithoutZeroes) {
+        int count = ZERO;
+        for (int number : arrayWithZeroes) {
+            if (number != ZERO) {
+                arrayWithoutZeroes[count] = number;
                 count++;
             }
         }
-        return arrayOfPrimes;
-    }
-
-    private static void fillArrayWithNumbers(int[] array, int firstNumber) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = firstNumber;
-            firstNumber++;
-        }
+        return arrayWithoutZeroes;
     }
 }
