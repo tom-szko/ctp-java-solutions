5555792ce8ef4594ecb07f83753cee59163aec89
Tomasz Szkopinski <tomasz.szkopinski@gmail.com>
2018-08-04 16:02:36 +0200

    Task #9 - improved variable and method naming - now the MARKER_VALUE can be replaced with numbers <= 0 and the logic of the program will still work as expected.

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -3,7 +3,7 @@ package pl.coderstrust.sieve;
 import java.util.Arrays;
 
 public class SieveOfEratosthenes {
-    private static final int ZERO = 0;
+    private static final int MARKER_VALUE = 0;
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sieve(123)));
@@ -11,39 +11,39 @@ public class SieveOfEratosthenes {
 
     private static int[] sieve(int maximumNumber) {
         int[] numbers = new int[maximumNumber - 1];
-        fillArrayWithNumbers(numbers, 2);
+        fillArrayWithValues(numbers, 2);
         for (int j = 2; j <= (int) Math.sqrt(maximumNumber); j++) {
             for (int k = j; k < maximumNumber - 1; k++) {
-                if (numbers[k] % j == ZERO) {
-                    numbers[k] = ZERO;
+                if (numbers[k] % j == 0) {
+                    numbers[k] = MARKER_VALUE;
                 }
             }
         }
-        int[] primes = new int[countNonZeroValues(numbers)];
-        return fillArrayWithNonZeroValues(numbers, primes);
+        int[] primes = new int[countNonMarkerValues(numbers)];
+        return fillArrayWithNonMarkerValues(numbers, primes);
     }
 
-    private static void fillArrayWithNumbers(int[] array, int number) {
+    private static void fillArrayWithValues(int[] array, int value) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = number;
-            number++;
+            array[i] = value;
+            value++;
         }
     }
 
-    private static int countNonZeroValues(int[] array) {
-        int numberOfPrimes = ZERO;
+    private static int countNonMarkerValues(int[] array) {
+        int numberOfPrimes = 0;
         for (int number : array) {
-            if (number != ZERO) {
+            if (number != MARKER_VALUE) {
                 numberOfPrimes++;
             }
         }
         return numberOfPrimes;
     }
 
-    private static int[] fillArrayWithNonZeroValues(int[] arrayWithZeroes, int[] arrayWithoutZeroes) {
-        int count = ZERO;
+    private static int[] fillArrayWithNonMarkerValues(int[] arrayWithZeroes, int[] arrayWithoutZeroes) {
+        int count = 0;
         for (int number : arrayWithZeroes) {
-            if (number != ZERO) {
+            if (number != MARKER_VALUE) {
                 arrayWithoutZeroes[count] = number;
                 count++;
             }
