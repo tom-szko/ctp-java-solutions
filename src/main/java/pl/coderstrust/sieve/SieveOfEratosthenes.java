a2a00a3136520e0931a752a2bab99407d3126dc3
Tomasz Szkopinski <tomasz.szkopinski@gmail.com>
2018-08-06 20:48:22 +0200

    Task 9 - corrected method names and parameter names - added markMultiples method - moved array initialization to initArray method + other minor adjustments in code according to the last bunch of comments

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -6,27 +6,29 @@ public class SieveOfEratosthenes {
     private static final int MARKER_VALUE = 0;
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sieve(123)));
+        System.out.println(Arrays.toString(sieve(127)));
     }
 
     private static int[] sieve(int maximumNumber) {
-        int[] numbers = new int[maximumNumber - 1];
-        fillArrayWithValues(numbers, 2);
+        int[] numbers = initArray(maximumNumber);
         for (int j = 2; j <= (int) Math.sqrt(maximumNumber); j++) {
-            for (int k = j; k < maximumNumber - 1; k++) {
-                if (numbers[k] % j == 0) {
-                    numbers[k] = MARKER_VALUE;
-                }
-            }
+            markMultiples(numbers, j);
         }
         return filterPrimes(numbers);
     }
 
-    private static void fillArrayWithValues(int[] array, int value) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = value;
-            value++;
+    private static void markMultiples(int[] array, int multiple) {
+        for (int nextMultiple = 2 * multiple; nextMultiple < array.length + 2; nextMultiple += multiple) {
+            array[nextMultiple - 2] = MARKER_VALUE;
         }
+    }
+
+    private static int[] initArray(int max) {
+        int[] result = new int[max - 1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i + 2;
+        }
+        return result;
     }
 
     private static int countPrimes(int[] array) {
@@ -39,10 +41,10 @@ public class SieveOfEratosthenes {
         return numberOfPrimes;
     }
 
-    private static int[] filterPrimes(int[] input) {
+    private static int[] filterPrimes(int[] array) {
         int count = 0;
-        int [] primes = new int[countPrimes(input)];
-        for (int number : input) {
+        int[] primes = new int[countPrimes(array)];
+        for (int number : array) {
             if (number != MARKER_VALUE) {
                 primes[count] = number;
                 count++;
