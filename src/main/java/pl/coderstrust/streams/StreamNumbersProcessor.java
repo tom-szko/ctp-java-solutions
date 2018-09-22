6d2d4884529a713016a73d293a5cae3e557f00c5
Tomasz Szkopinski <tomasz.szkopinski@gmail.com>
2018-09-22 12:59:36 +0200

    Task 26 Do you love streams - revised variable names - extracted lambda expression in forEach method to variable - removed try/catch blocks and added 'throws IOException' to method signature - renamed test txt files

--- src/main/java/pl/coderstrust/streams/StreamNumbersProcessor.java
+++ src/main/java/pl/coderstrust/streams/StreamNumbersProcessor.java
@@ -1,33 +1,29 @@
 package pl.coderstrust.streams;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.function.Consumer;
 
 public class StreamNumbersProcessor {
 
-    public void process(String fileName, String resultFileName) {
+    public void process(String fileName, String resultFileName) throws IOException {
         StringBuilder outputContents = new StringBuilder();
-        try {
-            Files.lines(Paths.get(fileName))
-                    .filter(s -> s.matches("^[0-9 ]+$"))
-                    .map(s -> Arrays.stream(s.split("\\s"))
-                            .filter(s1 -> s1.matches("[0-9]+"))
-                            .peek(s1 -> outputContents.append(s1).append("+"))
-                            .mapToInt(Integer::parseInt)
-                            .sum())
-                    .forEach(d -> outputContents.deleteCharAt(outputContents.length() - 1)
-                            .append(String.format("=%d\n", d)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter outputWriter = new PrintWriter(resultFileName)) {
-            outputWriter.print(outputContents);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        Consumer<Integer> appendSumAtEndOfFile = d -> outputContents.deleteCharAt(outputContents.length() - 1)
+                .append(String.format("=%d\n", d));
+        Files.lines(Paths.get(fileName))
+                .filter(line -> line.matches("^[0-9 ]+$"))
+                .map(unparsedNumbers -> Arrays.stream(unparsedNumbers.split("\\s"))
+                        .filter(parsedStringNumber -> parsedStringNumber.matches("[0-9]+"))
+                        .peek(parsedNumber -> outputContents.append(parsedNumber).append("+"))
+                        .mapToInt(Integer::parseInt)
+                        .sum())
+                .forEach(appendSumAtEndOfFile);
+        PrintWriter outputWriter = new PrintWriter(resultFileName);
+        outputWriter.print(outputContents);
+        outputWriter.flush();
+        outputWriter.close();
     }
 }
