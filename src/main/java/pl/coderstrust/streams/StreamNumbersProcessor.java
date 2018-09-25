c0ce9cc0825db9f636d75080b43cbd6d733a2d05
Tomasz Szkopinski <tomasz.szkopinski@gmail.com>
2018-09-26 01:09:12 +0200

    Task 26 streams: - extracted printing to file to private method - removed one of the map() methods from stream chain - revised names of variables in lambda expressions - added empty line before stream chain

--- src/main/java/pl/coderstrust/streams/StreamNumbersProcessor.java
+++ src/main/java/pl/coderstrust/streams/StreamNumbersProcessor.java
@@ -13,15 +13,19 @@ public class StreamNumbersProcessor {
         StringBuilder outputContents = new StringBuilder();
         Consumer<Integer> appendSumAtEndOfFile = sum -> outputContents.deleteCharAt(outputContents.length() - 1)
                 .append(String.format("=%d\n", sum));
+
         Files.lines(Paths.get(fileName))
                 .filter(line -> line.matches("^[0-9 ]+$"))
-                .map(unparsedNumbers -> Arrays.stream(unparsedNumbers.split("\\s"))
-                        .filter(parsedStringNumber -> parsedStringNumber.matches("[0-9]+"))
-                        .peek(parsedNumber -> outputContents.append(parsedNumber).append("+"))
+                .map(unparsedNumbers -> Arrays.stream(unparsedNumbers.trim().split("\\s+"))
+                        .peek(stringNumber -> outputContents.append(stringNumber).append("+"))
                         .mapToInt(Integer::parseInt)
                         .sum())
                 .forEach(appendSumAtEndOfFile);
-        PrintWriter outputWriter = new PrintWriter(resultFileName);
+        printToFile(outputContents, resultFileName);
+    }
+
+    private void printToFile(StringBuilder outputContents, String outputFileName) throws IOException {
+        PrintWriter outputWriter = new PrintWriter(outputFileName);
         outputWriter.print(outputContents);
         outputWriter.flush();
         outputWriter.close();
